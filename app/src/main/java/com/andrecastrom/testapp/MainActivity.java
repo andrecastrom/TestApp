655c2b978a202a4ca86db9909d7380753bf77e94
andrecastrom <andrecastromartinez@gmail.com>
2016-10-20 13:46:01 +0200

    Semana 2 - 3

--- app/src/main/java/com/andrecastrom/testapp/MainActivity.java
+++ app/src/main/java/com/andrecastrom/testapp/MainActivity.java
@@ -1,17 +1,45 @@
 package com.andrecastrom.testapp;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout sfiMiIndicadorRefresh;
+    ListView lstMiLista;
+    Adapter adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarFAB();
+
+        sfiMiIndicadorRefresh = (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadorRefresh);
+        lstMiLista = (ListView) findViewById(R.id.lstMiLista);
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+        sfiMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refrescandoContenido();
+            }
+        });
+    }
+
+    public void refrescandoContenido() {
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+        sfiMiIndicadorRefresh.setRefreshing(false);
     }
 
     public void agregarFAB() {
@@ -19,7 +47,17 @@ public class MainActivity extends AppCompatActivity {
         miFAB.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+                //SnackBar reemplaza a Toast
+                Snackbar.make(view, getResources().getString(R.string.mensaje), Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("SNACKBAR", "Click en Snackbar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorAccent))
+                        .show();
             }
         });
     }
